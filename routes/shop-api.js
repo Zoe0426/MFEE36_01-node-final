1e044a3db03d5bf28ccdc7d78eb72009d4b0b794
Shuyi <shuyi041@gmail.com>
2023-06-19 22:42:32 +0800

    s_get first five product data

--- routes/shop-api.js
+++ routes/shop-api.js
@@ -5,14 +5,18 @@ const upload = require(__dirname+"/../modules/img-upload.js");
 const multipartParser = upload.none(); 
 
 router.get ('/', async(req,res)=>{
-    const [data] = await db.query("SELECT * FROM address_book LIMIT 2");
-    const test="test"
-    data.forEach(i=>{
-        i.birthday = res.toDatetimeString(i.birthday)
-        i.created_at = res.toDatetimeString(i.created_at)
-    
-    });
-    res.json(data)
+    const sql=`SELECT p.*, MAX(ps.proDet_price) max_price, MIN(ps.proDet_price) min_price
+    FROM shop_pro p
+    JOIN shop_proDet ps ON p.pro_sid=ps.pro_sid
+    GROUP BY p.pro_sid
+    LIMIT 1, 5`
+    const [result]=await db.query(sql)
+    res.json({result})
+    // data.forEach(i=>{
+    //     i.birthday = res.toDatetimeString(i.birthday)
+    //     i.created_at = res.toDatetimeString(i.created_at)
+    // });
+    // res.json(data)
 })
 
 module.exports = router;
