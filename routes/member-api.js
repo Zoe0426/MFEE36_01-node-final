1288daf24b8150451d6b541196956a19dae061c4
Zoe0426 <90545407+Zoe0426@users.noreply.github.com>
2023-06-25 23:41:36 +0800

    會員修改會員修改api

--- routes/member-api.js
+++ routes/member-api.js
@@ -6,45 +6,14 @@ const db = require(__dirname + "/../modules/db_connect");
 const upload = require(__dirname + "/../modules/img-upload.js");
 const multipartParser = upload.none();
 
-// 會員密碼加鹽
+// 讀取會員資料
 router.get("/", async (req, res) => {
-  const [data] = await db.query("SELECT member_password FROM member_info LIMIT 7");
-
-  let sql_memSid = `SELECT MAX(member_sid) AS maxSid FROM member_info`;
-  let [result_memSid] = await db.query(sql_memSid);
-  res.json(result_memSid);
-
-  let new_memSid = "";
-  if (!result_memSid) {
-    new_memSid = "mem00001";
-  } else {
-    let currentCount = parseInt(result_memSid.maxSid.substr(3));
-    let newCount = currentCount + 1;
-    new_memSid = `mem${String(newCount).padStart(5, "0")}`;
-  }
-
-  const newData = data.map(async (v, i) => {
-    const saltRounds = 10;
-    const pwd = v.member_password;
-    const newPwd = await bcrypt.hash(pwd, saltRounds);
-    return { ...v, member_password: newPwd };
-  });
-
-  Promise.all(newData).then((result) => {
-    res.json(result);
-  });
-
-  // const saltRounds = 10;
-  // const myPassword = "password1";
-  // const testPassword = "password2";
-  // const myHash = "$2a$10$fok18OT0R/cWoR0a.VsjjuuYZV.XrfdYd5CpDWrYkhi1F0i8ABp6e"; // myPassword加密後結果(驗證用)
-
-  // bcrypt.hash(myPassword, saltRounds).then(function (hash) {
-  //     res.send(hash);
-  // });
+  const [data] = await db.query("SELECT * FROM member_info LIMIT 5");
+  res.json(data);
 });
 
-// 寫入加鹽密碼
+// --------------------------------------------
+// 新增會員資料
 
 /*
 1. sql insert
@@ -119,3 +88,45 @@ router.post("/", multipartParser, async (req, res) => {
   });
 });
 module.exports = router;
+
+// -----------------------------------
+// 修改會員資料
+router.put("/:sid", multipartParser, async (req, res) => {
+  let { sid } = req.params;
+
+  const sql = `UPDATE member_info SET 
+  member_name=?,
+  member_email=?,
+  member_password=?,
+  member_mobile=?,
+  member_gender=?,
+  member_birth=?,
+  member_pet=?,
+  member_level=?,
+  member_ID=?,
+  member_profile=?
+  WHERE member_sid='${sid}'`;
+
+  const saltRounds = 10;
+  let saltPwd = await bcrypt.hash(req.body.member_password, saltRounds);
+  console.log(saltPwd);
+
+  const [result] = await db.query(sql, [
+    req.body.member_name,
+    req.body.member_email,
+    saltPwd,
+    req.body.member_mobile,
+    req.body.member_gender,
+    req.body.member_birth,
+    req.body.member_pet,
+    req.body.member_level,
+    req.body.member_ID,
+    req.body.member_profile,
+    req.body.member_profile,
+  ]);
+
+  res.json({
+    success: !!result.changedRows,
+    result,
+  });
+});
