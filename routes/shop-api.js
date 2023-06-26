84d4fb899a1844ebdba2ee86ba565da4394df31c
Shuyi <shuyi041@gmail.com>
2023-06-26 17:49:35 +0800

    s_update a little

--- routes/shop-api.js
+++ routes/shop-api.js
@@ -4,7 +4,21 @@ const db = require(__dirname + "/../modules/db_connect")
 const upload = require(__dirname+"/../modules/img-upload.js");
 const multipartParser = upload.none(); 
 
-router.get ('/', async(req,res)=>{
+router.get ('/:cat', async(req,res)=>{
+const dict={
+    CAN:['category_detail_sid','CA'],
+    FEED:['category_detail_sid','FE'],
+    SNACK:['category_detail_sid','SN'],
+    HEALTH:['category_detail_sid','HE'],
+    DRESS:['category_detail_sid','DR'],
+    OUTDOOR:['category_detail_sid','OD'],
+    TOY:['category_detail_sid','TO'],
+    OTHER:['category_detail_sid','OT'],
+    DOG:['pro_for','D'],
+    CAT:['pro_for','C']
+}
+const {cat}=req.params
+
     const sql=`SELECT COUNT(1), p.*, MAX(ps.price) max_price, MIN(ps.price) min_price, ROUND(AVG(c.rating), 1) avg_rating
     FROM shop_product p
     LEFT JOIN shop_product_detail ps ON p.product_sid=ps.product_sid
@@ -12,7 +26,7 @@ router.get ('/', async(req,res)=>{
     GROUP BY p.product_sid
     LIMIT 1, 1000`
     const [result]=await db.query(sql)
-    res.json(result)
+    res.json({result})
     // data.forEach(i=>{
     //     i.birthday = res.toDatetimeString(i.birthday)
     //     i.created_at = res.toDatetimeString(i.created_at)
