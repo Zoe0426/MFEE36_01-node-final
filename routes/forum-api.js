69834bb48296dc9940c5d30c4f72bac0dca0c0b0
Lilian-mac <lilian4218@gmail.com>
2023-07-12 15:49:55 +0800

    first

--- routes/forum-api.js
+++ routes/forum-api.js
@@ -5,7 +5,12 @@ const upload = require(__dirname+"/../modules/img-upload.js");
 const multipartParser = upload.none(); 
 
 router.get ('/', async(req,res)=>{
-    const [data] = await db.query("SELECT * FROM address_book LIMIT 2");
+    const [data] = await db.query("SELECT * FROM `post_list_member` WHERE 1 LIMIT 2");
+    res.json(data)
+    //阿鄉到此一遊
+})
+router.get ('/:postSid', async(req,res)=>{
+    const [data] = await db.query("SELECT * FROM `post_list_member` WHERE 1 LIMIT 2");
     res.json(data)
     //阿鄉到此一遊
 })
