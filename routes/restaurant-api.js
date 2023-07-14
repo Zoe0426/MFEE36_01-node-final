124cf97b3dc382424fe32c2930777f4a90989671
Jill <>
2023-07-14 09:02:04 +0800

    rest_api

--- routes/restaurant-api.js
+++ routes/restaurant-api.js
@@ -6,7 +6,11 @@ const multipartParser = upload.none();
 
 router.get("/", async (req, res) => {
   const sql =
-    "SELECT r.name, r.city, r.area, ru.rule_name, s.service_name FROM restaurant_information AS r JOIN restaurant_associated_rule AS ar ON r.rest_sid = ar.rest_sid JOIN restaurant_rule AS ru ON ar.rule_sid = ru.rule_sid JOIN restaurant_associated_service AS asr ON r.rest_sid = asr.rest_sid JOIN restaurant_service AS s ON asr.service_sid = s.service_sid;";
+    `SELECT r.name, r.city, r.area, ru.rule_name, s.service_name 
+    FROM restaurant_information AS r 
+    JOIN restaurant_associated_rule AS ar ON r.rest_sid = ar.rest_sid 
+    JOIN restaurant_rule AS ru ON ar.rule_sid = ru.rule_sid 
+    JOIN restaurant_associated_service AS asr ON r.rest_sid = asr.rest_sid JOIN restaurant_service AS s ON asr.service_sid = s.service_sid;`;
   const [data] = await db.query(sql);
   res.json(data);
 });
