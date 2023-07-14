8e847c20420c958846fac88ec54ffd633c098390
jill <jillwunnie1213@gmail.com>
2023-07-14 14:22:03 +0800

    rest_api

--- routes/restaurant-api.js
+++ routes/restaurant-api.js
@@ -7,15 +7,16 @@ const multipartParser = upload.none();
 router.get("/", async (req, res) => {
   const sql = `SELECT r.rest_sid, r.name, r.city, r.area, 
   GROUP_CONCAT(DISTINCT ru.rule_name) AS rule_names, 
-  GROUP_CONCAT(DISTINCT s.service_name) AS service_names 
+  GROUP_CONCAT(DISTINCT s.service_name) AS service_names, 
+  GROUP_CONCAT(DISTINCT ri.img_name) AS img_names 
   FROM restaurant_information AS r 
   JOIN restaurant_associated_rule AS ar ON r.rest_sid = ar.rest_sid 
   JOIN restaurant_rule AS ru ON ar.rule_sid = ru.rule_sid 
   JOIN restaurant_associated_service AS asr ON r.rest_sid = asr.rest_sid 
   JOIN restaurant_service AS s ON asr.service_sid = s.service_sid 
+  JOIN restaurant_img AS ri ON r.rest_sid = ri.rest_sid 
   GROUP BY r.rest_sid, r.name, r.city, r.area 
-  HAVING COUNT(DISTINCT ru.rule_name) > 1 OR COUNT(DISTINCT s.service_name) > 1;
-  `;
+  HAVING COUNT(DISTINCT ru.rule_name) > 1 OR COUNT(DISTINCT s.service_name) > 1;`;
   const [data] = await db.query(sql);
   res.json(data);
 });
