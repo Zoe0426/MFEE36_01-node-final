383a41e8b118779c9f1f4b518f3ce95d94c66ce4
Jill <>
2023-07-14 13:06:20 +0800

    rest_api

--- routes/restaurant-api.js
+++ routes/restaurant-api.js
@@ -5,15 +5,20 @@ const upload = require(__dirname + "/../modules/img-upload.js");
 const multipartParser = upload.none();
 
 router.get("/", async (req, res) => {
-  const sql =
-    `SELECT r.name, r.city, r.area, ru.rule_name, s.service_name 
-    FROM restaurant_information AS r 
-    JOIN restaurant_associated_rule AS ar ON r.rest_sid = ar.rest_sid 
-    JOIN restaurant_rule AS ru ON ar.rule_sid = ru.rule_sid 
-    JOIN restaurant_associated_service AS asr ON r.rest_sid = asr.rest_sid JOIN restaurant_service AS s ON asr.service_sid = s.service_sid;`;
+  const sql = `SELECT r.rest_sid, r.name, r.city, r.area, 
+  GROUP_CONCAT(DISTINCT ru.rule_name) AS rule_names, 
+  GROUP_CONCAT(DISTINCT s.service_name) AS service_names 
+  FROM restaurant_information AS r 
+  JOIN restaurant_associated_rule AS ar ON r.rest_sid = ar.rest_sid 
+  JOIN restaurant_rule AS ru ON ar.rule_sid = ru.rule_sid 
+  JOIN restaurant_associated_service AS asr ON r.rest_sid = asr.rest_sid 
+  JOIN restaurant_service AS s ON asr.service_sid = s.service_sid 
+  GROUP BY r.rest_sid, r.name, r.city, r.area 
+  HAVING COUNT(DISTINCT ru.rule_name) > 1 OR COUNT(DISTINCT s.service_name) > 1;
+  `;
   const [data] = await db.query(sql);
   res.json(data);
 });
-//測試
+
 module.exports = router;
 console.log(JSON.stringify(router, null, 4));
