26322d6452ba019a177c8fa2efad89f264a669b5
Lilian-mac <lilian4218@gmail.com>
2023-07-18 23:14:50 +0800

    加了post_id的api

--- routes/forum-api.js
+++ routes/forum-api.js
@@ -79,7 +79,7 @@ router.get ('/index_try', async(req,res)=>{
     res.json({data, lateD});
     
 });
-
+// 首頁下面：你可能會喜歡
 router.get ('/recommend', async(req,res)=>{
     const [data] = await db.query(
         `SELECT
@@ -117,14 +117,45 @@ router.get ('/recommend', async(req,res)=>{
 // 文章點進去的頁面
 router.get ('/:postid', async(req,res)=>{
     //console.log('req.params:', req.params.postid);
-   let {postid} = req.params;
-    const [data] = await db.query(`SELECT post_list_member.*,post_comment.*,post_board.* FROM post_list_member JOIN post_comment ON post_list_member.post_sid=post_comment.post_sid JOIN post_board ON post_list_member.board_sid = post_board.board_sid WHERE post_list_member.post_sid='${postid}'`);
+    let {postid} = req.params;
+    const [data] = await db.query(`
+    SELECT 
+    plm.post_title, 
+    plm.post_content, 
+    mi.nickname, 
+    mi.member_ID, 
+    pb.board_name, 
+    pb.board_img, 
+    plm.post_date, 
+    plm.update_date, 
+    ph.hashtag_name, 
+    pf.file, 
+    pc.comment_content, 
+    pc.comment_date,
+    (SELECT COUNT(1) FROM post_like pl WHERE pl.post_sid = plm.post_sid) AS postLike, 
+    (SELECT COUNT(1) FROM post_comment pc WHERE pc.post_sid = plm.post_sid) AS postComment
+FROM 
+    post_list_member plm 
+JOIN 
+    member_info mi ON mi.member_sid = plm.member_sid
+JOIN 
+    post_board pb ON plm.board_sid = pb.board_sid
+JOIN 
+    post_comment pc ON plm.post_sid = pc.post_sid
+JOIN 
+    post_hashtag ph ON plm.post_sid = ph.post_sid
+JOIN 
+    post_like pl ON plm.post_sid = pl.post_sid
+JOIN 
+    post_file pf ON pf.post_sid = plm.post_sid
+WHERE plm.post_sid='${postid}'`)
+    // const [data] = await db.query(`SELECT post_list_member.*,post_comment.*,post_board.* FROM post_list_member JOIN post_comment ON post_list_member.post_sid=post_comment.post_sid JOIN post_board ON post_list_member.board_sid = post_board.board_sid WHERE post_list_member.post_sid='${postid}'`);
     console.log('data', data);
     res.json(data)
     //res.send('124');
 })
 
-// 看板 問題：為什麼跑出的資料沒有只有board_sid=1(有在資料庫跑過的)
+// 看板
 router.get ('/board/:boardid', async(req,res)=>{
     let {boardid} = req.params;
     const [boardData] = await db.query(
