b38c88325c6f66ea4764c4684fe0dc9858e71f77
Lilian-mac <lilian4218@gmail.com>
2023-07-20 15:54:06 +0800

    試試blog api

--- routes/forum-api.js
+++ routes/forum-api.js
@@ -1,15 +1,15 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const db = require(__dirname + "/../modules/db_connect")
-const upload = require(__dirname+"/../modules/img-upload.js");
-const multipartParser = upload.none(); 
+const db = require(__dirname + "/../modules/db_connect");
+const upload = require(__dirname + "/../modules/img-upload.js");
+const multipartParser = upload.none();
 
 // 論壇首頁
-router.get ('/', async(req,res)=>{
-    // 論壇文章列表
-    // 熱門文章(按讚數)
-    const [data] = await db.query(
-        `
+router.get("/", async (req, res) => {
+  // 論壇文章列表
+  // 熱門文章(按讚數)
+  const [data] = await db.query(
+    `
         SELECT mi.member_sid, mi.nickname, plm.post_sid, plm.board_sid, plm.post_title, 
         CASE WHEN CHAR_LENGTH(plm.post_content) > 70 THEN CONCAT(SUBSTRING(plm.post_content, 1, 70), '...') 
         ELSE plm.post_content END AS post_content, pb.board_name, 
@@ -21,18 +21,17 @@ router.get ('/', async(req,res)=>{
         JOIN post_board pb ON plm.board_sid = pb.board_sid 
         ORDER BY postLike DESC;
       `
-    );
+  );
 
-    res.json(data);
-    
+  res.json(data);
 });
 
 // 論壇首頁try try 看
-router.get ('/index_try', async(req,res)=>{
-    // 論壇文章列表
-    // 熱門文章(按讚數)
-    const [data] = await db.query(
-        `
+router.get("/index_try", async (req, res) => {
+  // 論壇文章列表
+  // 熱門文章(按讚數)
+  const [data] = await db.query(
+    `
         SELECT mi.member_sid, mi.nickname, plm.post_sid, plm.board_sid, plm.post_title, 
         CASE WHEN CHAR_LENGTH(plm.post_content) > 70 THEN CONCAT(SUBSTRING(plm.post_content, 1, 70), '...') 
         ELSE plm.post_content END AS post_content, pb.board_name, 
@@ -44,10 +43,10 @@ router.get ('/index_try', async(req,res)=>{
         JOIN post_board pb ON plm.board_sid = pb.board_sid 
         ORDER BY postLike DESC;
       `
-    );
-    // 最新文章（按日期）
-    const [lateD] = await db.query(
-        `
+  );
+  // 最新文章（按日期）
+  const [lateD] = await db.query(
+    `
         SELECT mi.member_sid, mi.nickname, plm.post_sid, plm.board_sid, plm.post_title, plm.post_date, 
         CASE WHEN CHAR_LENGTH(plm.post_content) > 70 THEN CONCAT(SUBSTRING(plm.post_content, 1, 70), '...') 
         ELSE plm.post_content END AS post_content, pb.board_name, 
@@ -59,15 +58,14 @@ router.get ('/index_try', async(req,res)=>{
         JOIN post_board pb ON plm.board_sid = pb.board_sid 
         ORDER BY post_date DESC;
         `
-    )
+  );
 
-    res.json({data, lateD});
-    
+  res.json({ data, lateD });
 });
 // 首頁下面：你可能會喜歡
-router.get ('/recommend', async(req,res)=>{
-    const [data] = await db.query(
-        `SELECT
+router.get("/recommend", async (req, res) => {
+  const [data] = await db.query(
+    `SELECT
         mi.member_sid,
         mi.nickname,
         plm.post_sid,
@@ -89,17 +87,16 @@ router.get ('/recommend', async(req,res)=>{
     LIMIT 1,10;
     
     `
-    );
-    res.json(data)
-    
-})
+  );
+  res.json(data);
+});
 
 // 文章點進去的頁面(postid)
 // 1. 上半部：作者、文章標題
-router.get ('/:postid', async(req,res)=>{
-    //console.log('req.params:', req.params.postid);
-    let {postid} = req.params;
-    const [data] = await db.query(`
+router.get("/:postid", async (req, res) => {
+  //console.log('req.params:', req.params.postid);
+  let { postid } = req.params;
+  const [data] = await db.query(`
     SELECT 
     plm.post_title, 
     plm.post_content, 
@@ -118,58 +115,86 @@ JOIN
     member_info mi ON mi.member_sid = plm.member_sid
 JOIN 
     post_board pb ON plm.board_sid = pb.board_sid
-WHERE plm.post_sid='${postid}'`)
-    console.log('data', data);
+WHERE plm.post_sid='${postid}'`);
+  // console.log('data', data);
+  const newData = data.map((v) => ({
+    ...v,
+    post_date: res.toDatetimeString(v.post_date),
+    update_date: res.toDatetimeString(v.update_date),
+  }));
+  console.log("newData", newData);
 
-// 2. hashtag
-    const [tagData] = await db.query(
-        `
+  // 2. hashtag
+  const [tagData] = await db.query(
+    `
         SELECT ph.hashtag_name FROM post_hashtag ph 
         JOIN post_list_member plm ON plm.post_sid = ph.post_sid 
         WHERE plm.post_sid = '${postid}';
         `
-    )
-    console.log('tagData',tagData);
+  );
+  console.log("tagData", tagData);
 
-// 3. 文章下面：留言
-    const [commentData] = await db.query(
-        `
+  // 3. 文章下面：留言
+  const [commentData] = await db.query(
+    `
         SELECT pc.comment_content, pc.comment_date, pc.member_sid, mi.nickname, mi.member_sid, mi.profile FROM post_comment pc 
         JOIN member_info mi ON mi.member_sid = pc.member_sid
         JOIN post_list_member plm ON plm.post_sid = pc.post_sid
         WHERE plm.post_sid = '${postid}';
         `
-    )
-    console.log('commentData',commentData);
+  );
+  const newCommentData = commentData.map((v) => ({
+    ...v,
+    comment_date: res.toDatetimeString(v.comment_date),
+  }));
+  console.log("commentData", commentData);
+  console.log("newCommentData", newCommentData);
 
-// 4. 圖片
-    const [imgData] = await db.query(
-        `
+  // 4. 圖片
+  const [imgData] = await db.query(
+    `
         SELECT pf.file, plm.post_sid FROM post_file pf 
         JOIN post_list_member plm ON pf.post_sid = plm.post_sid
         WHERE pf.post_sid = '${postid}';
         `
-    )
-    console.log('imgData',imgData);
+  );
+  console.log("imgData", imgData);
 
-
-    res.json({data, tagData, commentData, imgData});
-
-})
+  res.json({ newData, tagData, newCommentData, imgData });
+});
 
 // 看板
-router.get ('/board/:boardid', async(req,res)=>{
-    let {boardid} = req.params;
-    const [boardData] = await db.query(
-        `SELECT plm.*, pc.*, pb.* 
+router.get("/board/:boardid", async (req, res) => {
+  let { boardid } = req.params;
+  const [boardData] = await db.query(
+    `SELECT plm.*, pc.*, pb.* 
             FROM post_list_member plm 
             JOIN post_comment pc
             ON plm.post_sid = pc.post_sid
             JOIN post_board pb
             ON plm.board_sid = pb.board_sid 
             WHERE pb.board_sid = '${boardid}'`
-        );
-    res.json(boardData)
-})
+  );
+  res.json(boardData);
+});
+
+// 個人頁面：我的文章
+router.get("/blogPost", async (req, res) => {
+  const [blogPostData] = await db.query(
+    `SELECT mi.member_sid, mi.nickname, plm.post_sid, plm.board_sid, plm.post_title, plm.post_date, 
+        CASE WHEN CHAR_LENGTH(plm.post_content) > 70 THEN CONCAT(SUBSTRING(plm.post_content, 1, 70), '...') 
+        ELSE plm.post_content END AS post_content, pb.board_name, 
+        (SELECT file FROM post_file pfile WHERE pfile.post_sid = plm.post_sid ORDER BY pfile.file_type LIMIT 1) AS file, 
+        (SELECT COUNT(1) FROM post_like pl WHERE pl.post_sid = plm.post_sid) AS postLike, 
+        (SELECT COUNT(1) FROM post_comment pc WHERE pc.post_sid = plm.post_sid) AS postComment, 
+        (SELECT COUNT(1) FROM post_favlist pf WHERE pf.post_sid = plm.post_sid) AS postFavlist 
+        FROM post_list_member plm JOIN member_info mi ON mi.member_sid = plm.member_sid 
+        JOIN post_board pb ON plm.board_sid = pb.board_sid 
+        WHERE mi.member_sid = "mem00300" 
+        ORDER BY post_date DESC;`
+  );
+  res.json(blogPostData);
+  console.log("blogPostData", blogPostData);
+});
 
 module.exports = router;
