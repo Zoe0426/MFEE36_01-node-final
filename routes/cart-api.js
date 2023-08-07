e2bfc7235a50a68e4fa1963dfc46529c26b1668f
Lily Kuo <ilwitulily0209@gmail.com>
2023-08-07 16:13:34 +0800

    update repay sql

--- routes/cart-api.js
+++ routes/cart-api.js
@@ -259,6 +259,64 @@ const paymentSucceeded=async(data,res)=>{
 const paymentFailed = async()=>{
     //res.redirect(`http://localhost:3000/cart/order-complete?orderSid=${CustomField1}&checkoutType=${CustomField2}&memberSid=${CustomField3}`);
 }
+const getOrderDetail = async(orderSid)=>{
+    const output = {checkoutType:'', details:[]}
+ try{
+        const getOrderDetailSql = `SELECT
+                od.order_detail_sid as order_detail_sid,
+                sp.product_sid as rel_sid,
+                sp.name as rel_name,
+                spd.product_detail_sid as rel_seq_sid,
+                spd.name as rel_seq_name,
+                spd.price as prod_price,
+                od.product_qty as prod_qty,
+                od.rel_type as rel_type,
+                0 as adult_price,
+                0 as child_price,
+                0 as adult_qty,
+                0 as child_qty,
+                sp.img as img
+            FROM
+                order_details od
+                JOIN shop_product sp ON od.rel_sid = sp.product_sid
+                JOIN shop_product_detail spd ON sp.product_sid = spd.product_sid
+                AND od.rel_seq_sid = spd.product_detail_sid
+            WHERE
+                od.order_sid = ?
+            UNION
+            ALL
+            SELECT
+                od.order_detail_sid as order_detail_sid,
+                ai.activity_sid as rel_sid,
+                ai.name as rel_name,
+                ag.activity_group_sid as rel_seq_sid,
+                ag.date as rel_seq_name,
+                0 as prod_price,
+                0 as prod_qty,
+                od.rel_type as rel_type,
+                ag.price_adult as adult_price,
+                ag.price_kid as child_price,
+                od.adult_qty as adult_qty,
+                od.child_qty as child_qty,
+                ai.activity_pic as img
+            FROM
+                order_details od
+                JOIN activity_info ai ON od.rel_sid = ai.activity_sid
+                JOIN activity_group ag ON ai.activity_sid = ag.activity_sid
+                AND od.rel_seq_sid = ag.activity_group_sid
+            WHERE
+                od.order_sid = ?`;
+        const [details] = await db.query(getOrderDetailSql, [orderSid,orderSid]);
+        output.details = details;
+        output.checkoutType = details[0].rel_type;
+        
+
+    }catch(error){
+        console.error(error);
+        throw new Error('取商品資料時出錯');
+    }
+    return output;
+}
 router.post ('/get-cart-items', async(req,res)=>{
     let output ={
         shop : [],
@@ -526,6 +584,74 @@ router.get('/get-home-data', async(req,res)=>{
     }catch(error){
         console.error(error);
         throw new Error('取貼文資料時出錯');
+    }
+    res.json(output);
+})
+router.get('/get-repay-order/:orderSid', async(req,res)=>{
+     let output ={
+        shop : [],
+        activity : [],
+        postAddress:[],
+        coupon:[],
+    }
+    const orderSid  = req.params.orderSid;
+   
+    //拿取訂單商品/活動細節
+    getOrderDetail(orderSid);
+   
+    // if(checkoutType === )
+        try{
+        const getOrderDetailSql = `SELECT
+                od.order_detail_sid as order_detail_sid,
+                sp.product_sid as rel_sid,
+                sp.name as rel_name,
+                spd.product_detail_sid as rel_seq_sid,
+                spd.name as rel_seq_name,
+                spd.price as prod_price,
+                od.product_qty as prod_qty,
+                od.rel_type as rel_type,
+                0 as adult_price,
+                0 as child_price,
+                0 as adult_qty,
+                0 as child_qty,
+                sp.img as img
+            FROM
+                order_details od
+                JOIN shop_product sp ON od.rel_sid = sp.product_sid
+                JOIN shop_product_detail spd ON sp.product_sid = spd.product_sid
+                AND od.rel_seq_sid = spd.product_detail_sid
+            WHERE
+                od.order_sid = ?
+            UNION
+            ALL
+            SELECT
+                od.order_detail_sid as order_detail_sid,
+                ai.activity_sid as rel_sid,
+                ai.name as rel_name,
+                ag.activity_group_sid as rel_seq_sid,
+                ag.date as rel_seq_name,
+                0 as prod_price,
+                0 as prod_qty,
+                od.rel_type as rel_type,
+                ag.price_adult as adult_price,
+                ag.price_kid as child_price,
+                od.adult_qty as adult_qty,
+                od.child_qty as child_qty,
+                ai.activity_pic as img
+            FROM
+                order_details od
+                JOIN activity_info ai ON od.rel_sid = ai.activity_sid
+                JOIN activity_group ag ON ai.activity_sid = ag.activity_sid
+                AND od.rel_seq_sid = ag.activity_group_sid
+            WHERE
+                od.order_sid = ?`;
+        const [details] = await db.query(getOrderDetailSql, [orderSid,orderSid]);
+        console.log({details});
+        const checkoutType = details[0].rel_type;
+        console.log(checkoutType)
+    }catch(error){
+        console.error(error);
+        throw new Error('取商品資料時出錯');
     }
     res.json(output);
 })
